43e163b6395811d7ba5a00240cee8b932a433e25
MdRashid838 <160717010+MdRashid838@users.noreply.github.com>
2025-11-28 04:04:19 +0530

    addnew

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
-const {connectMongoDB} = require('./src/connection');
+const connectMongoDB = require('./src/connection');
 
 const Restaurant =  require('./src/routes/restaurantRouter');
 const menuItem =  require('./src/routes/menuItemRouter');
--- src/connection.js
+++ src/connection.js
@@ -12,22 +12,21 @@ mongoose.set("strictQuery", true);
 
 async function connectMongoDB() {
   try {
-    const dbUrl = process.env.MONGO_URL;  // env से लेगा
+    const dbUrl = process.env.MONGO_URL;
+
     if (!dbUrl) {
-      console.error(" MONGO_URL is missing in environment variables");
+      console.error(" MONGO_URL is missing in .env file");
       return;
     }
 
-    await mongoose.connect(dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(dbUrl);
 
-    console.log(" MongoDB Connected!");
+    console.log(" MongoDB Atlas Connected Successfully!");
   } catch (err) {
-    console.error(" MongoDB Connection Error:", err);
+    console.error(" MongoDB Connection Error:", err.message);
     process.exit(1);
   }
 }
 
 module.exports = connectMongoDB;
+
