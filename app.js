7ec8a7d518bfad019d0a625b23988240b0645b5f
MdRashid838 <160717010+MdRashid838@users.noreply.github.com>
2025-12-04 01:22:07 +0530

    add backend url

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://www-swiggy-clone-mq7x.onrender.com/",
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));;
